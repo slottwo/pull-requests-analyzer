3d6e0f568f621d14a7dc1cb3960d018ca4334e53
Pedro [kaled] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2020-08-11 16:54:45 -0300

    Add features in sketch.py
    Ignore CSV files in gitignore

--- sketch.py
+++ sketch.py
@@ -10,11 +10,34 @@ total_merges = 0
 total_rebases = 0
 total_not_merged_pr = 0
 
+output = dict()
+
 g = github.Github(token)
 
 for ownerrepo in ownerrepo_ls:
+    output[ownerrepo] = {}
+
     repo = g.get_repo(ownerrepo)
     pulls = repo.get_pulls(state='closed')
+    
     for pr in pulls:
-        if pr.merged_at != None:
-            total_rebases += 1
+        if pr.is_merged:
+            output[ownerrepo][pr.id] = {
+                'merge_commit_sha': pr.merge_commit_sha,
+                'base_sha': pr.base.sha,
+                'data': pr.merged_at
+            }
+            if len(repo.get_commit(pr.merge_commit_sha).commit.parents) > 1:
+                output[ownerrepo][pr.id]['merge_or_rebase'] = 'merge'
+                total_merges += 1
+            else:
+                total_rebases += 1
+                output[ownerrepo][pr.id]['merge_or_rebase'] = 'rebase'
+        else:
+            total_not_merged_pr += 1
+
+with open('output_by_repo.csv', 'w', newline='') as csvfile:
+    pass
+
+with open('output_by_repo.csv', 'w', newline='') as csvfile:
+    pass
