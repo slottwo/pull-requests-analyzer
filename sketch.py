86949f714371a8d4c1b55ff84c8e17a173ffcbc5
Pedro [kaled] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2020-08-11 18:26:42 -0300

    Output implementation

--- sketch.py
+++ sketch.py
@@ -2,42 +2,46 @@ import github
 import csv
 
 with open('input.csv', newline='') as csvfile:
-    reader = list(csv.reader(csvfile))
-    token = reader[0][0]
-    ownerrepo_ls = tuple(reader[1:])
-
-total_merges = 0
-total_rebases = 0
-total_not_merged_pr = 0
-
-output = dict()
+    csv_reader = list(csv.reader(csvfile))
+    token = csv_reader[0][0]
+    ownerrepo_ls = tuple(csv_reader[1:])
+    del csv_reader
 
 g = github.Github(token)
 
 for ownerrepo in ownerrepo_ls:
-    output[ownerrepo] = {}
+    output = {'total_merges': 0, 'total_rebases': 0, 'total_not_merged_pr': 0}
 
     repo = g.get_repo(ownerrepo)
     pulls = repo.get_pulls(state='closed')
     
     for pr in pulls:
         if pr.is_merged:
-            output[ownerrepo][pr.id] = {
+            output[pr.id] = {
                 'merge_commit_sha': pr.merge_commit_sha,
                 'base_sha': pr.base.sha,
                 'data': pr.merged_at
             }
             if len(repo.get_commit(pr.merge_commit_sha).commit.parents) > 1:
-                output[ownerrepo][pr.id]['merge_or_rebase'] = 'merge'
-                total_merges += 1
+                output[pr.id]['merge_or_rebase'] = 'merge'
+                output['total_merges'] += 1
             else:
-                total_rebases += 1
-                output[ownerrepo][pr.id]['merge_or_rebase'] = 'rebase'
+                output[pr.id]['merge_or_rebase'] = 'rebase'
+                output['total_rebases'] += 1
         else:
-            total_not_merged_pr += 1
+            output['total_not_merged_pr'] += 1
+    
+    with open('output_by_repo.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow([])
+        del csvfile
 
-with open('output_by_repo.csv', 'w', newline='') as csvfile:
-    pass
-
-with open('output_by_repo.csv', 'w', newline='') as csvfile:
-    pass
+    with open('output_total.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow([
+            ownerrepo,
+            output['total_merges'],
+            output['total_rebases'],
+            output['total_not_merged_pr']
+        ])
+        del csvwriter
