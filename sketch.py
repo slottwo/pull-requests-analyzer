3e5dea7353bb5b4f8ec7ae47bf8681ea9e8b6e72
Pedro [kaled] H. S. F. Galvão <41028091+slottwo@users.noreply.github.com>
2020-08-12 00:12:29 -0300

    Bug fixes

--- sketch.py
+++ sketch.py
@@ -4,44 +4,50 @@ import csv
 with open('input.csv', newline='') as csvfile:
     csv_reader = list(csv.reader(csvfile))
     token = csv_reader[0][0]
-    ownerrepo_ls = tuple(csv_reader[1:])
+    ownerrepo_ls = tuple(csv_reader[1:][0])
     del csv_reader
+print(token)
+print(ownerrepo_ls)
 
 g = github.Github(token)
 
 for ownerrepo in ownerrepo_ls:
-    output = {'total_merges': 0, 'total_rebases': 0, 'total_not_merged_pr': 0}
+    pr_analysis = dict()
+    total_pr = {'total_merges': 0, 'total_rebases': 0, 'total_not_merged_pr': 0}
 
     repo = g.get_repo(ownerrepo)
     pulls = repo.get_pulls(state='closed')
     
     for pr in pulls:
         if pr.is_merged:
-            output[pr.id] = {
+            pr_analysis[pr.id] = {
                 'merge_commit_sha': pr.merge_commit_sha,
                 'base_sha': pr.base.sha,
                 'data': pr.merged_at
             }
             if len(repo.get_commit(pr.merge_commit_sha).commit.parents) > 1:
-                output[pr.id]['merge_or_rebase'] = 'merge'
-                output['total_merges'] += 1
+                pr_analysis[pr.id]['merge_or_rebase'] = 'merge'
+                total_pr['total_merges'] += 1
             else:
-                output[pr.id]['merge_or_rebase'] = 'rebase'
-                output['total_rebases'] += 1
+                pr_analysis[pr.id]['merge_or_rebase'] = 'rebase'
+                total_pr['total_rebases'] += 1
         else:
-            output['total_not_merged_pr'] += 1
-    
-    with open('output_by_repo.csv', 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow([])
-        del csvfile
+            total_pr['total_not_merged_pr'] += 1
 
-    with open('output_total.csv', 'w', newline='') as csvfile:
+    with open(ownerrepo.replace('/', '-') + '.csv', 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['merged_commit_sha', 'base_sha', 'date', 'merge_or_rebase'])
+        for pr in pr_analysis.values():
+            csvwriter.writerow(list(pr.values()))
+        del csvfile
+    
+    with open('total_pr_analysis.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['owner/repo','total_merges','total_rebases','total_not_merged_pull_requests'])
         csvwriter.writerow([
             ownerrepo,
-            output['total_merges'],
-            output['total_rebases'],
-            output['total_not_merged_pr']
+            total_pr['total_merges'],
+            total_pr['total_rebases'],
+            total_pr['total_not_merged_pr']
         ])
         del csvwriter
